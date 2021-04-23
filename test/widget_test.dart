84120a31fa18dda7eadab8d945a305ac59e7292c
Ryan Webster <websterz77@hotmail.com>
2021-04-23 19:25:13 -0400

    Update v1

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mobile_app/Screens/calculator.dart';
 
-import 'package:mobile_app/main.dart';
-
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
